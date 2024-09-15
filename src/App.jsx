66d14b1d613b945dc017bfe34fabc880f5f6c085
Daniel Rojas Grass <drgrassnk445@gmail.com>
2024-09-15 11:32:06 +0200

    ok

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import PortfolioPreview from "./Components/firstPage";
+
+function App() {
+  return (
+    <div>
+      <PortfolioPreview />
+    </div>
+  );
+}
+
+export default App;
