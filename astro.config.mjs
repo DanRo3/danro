97deb4a8a4fe9104e561c55de9cffe8ca20c7869
Daniel Rojas Grass <drgrassnk445@gmail.com>
2024-10-23 09:23:54 -0400

    ok

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://danro3.github.io/danro/'
+  site: 'https://danro3.github.io/'
 })
